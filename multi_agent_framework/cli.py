1bb37761300f3abbf7468f528709c7f7c135be7c
Michael Goetz <mpgoetz@gmail.com>
2025-06-13 16:25:49 -0500

    fix: Fix import error in CLI for get_available_agents

--- multi_agent_framework/cli.py
+++ multi_agent_framework/cli.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from multi_agent_framework.core.project_config import ProjectConfig
 from multi_agent_framework.core.message_bus_configurable import MessageBus
-from multi_agent_framework.core.agent_factory import create_agent, get_available_agents
+from multi_agent_framework.core.agent_factory import create_agent, AgentFactory
 from multi_agent_framework import config
 
 
